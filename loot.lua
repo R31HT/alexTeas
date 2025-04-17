54459605456fbc8019a8794f96decf96d3430b51
R310 <105254128+R31HT@users.noreply.github.com>
2025-04-17 17:28:35 +0400

    Create loot.lua

---
+++ loot.lua
@@ -0,0 +1,18 @@
+local player = game.Players.LocalPlayer
+local backpack = player:WaitForChild("Backpack")
+local humanoid = player.Character:WaitForChild("Humanoid")
+
+for _, tool in ipairs(game:GetDescendants()) do
+	if tool:IsA("Tool") and tool.Parent ~= backpack then
+		local owned = false
+		for _, plr in ipairs(game.Players:GetPlayers()) do
+			if (plr.Character and tool:IsDescendantOf(plr.Character)) or (plr:FindFirstChild("Backpack") and tool:IsDescendantOf(plr.Backpack)) then
+				owned = true
+				break
+			end
+		end
+		if not owned then
+			replicatesignal(humanoid.ServerEquipTool, tool)
+		end
+	end
+end
